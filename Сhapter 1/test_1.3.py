eec5b56b9ced056f3e07eaa1cf9812ee8e12f0a1
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2024-05-03 10:30:42 +0300

    new files test 1.3 v2

---
+++ Сhapter 1/test_1.3.py
@@ -0,0 +1,21 @@
+digit_1 = float(input())
+digit_2 = float(input())
+operation = input()
+
+if operation == "+":
+    print(digit_1 + digit_2)
+elif operation == "-":
+    print(digit_1 - digit_2)
+elif operation == "*":
+    print(digit_1 * digit_2)
+elif operation == "pow":
+    print(digit_1 ** digit_2)
+
+if operation == "div" and (digit_2 != 0):
+    print(digit_1 // digit_2)
+elif operation == "/" and (digit_2 != 0):
+    print(digit_1 / digit_2)
+elif operation == "mod" and (digit_2 != 0):
+    print(digit_1 % digit_2)
+elif (operation == "div" or operation == "/" or operation == "mod") and (digit_2 == 0):
+    print("Деление на 0!")
