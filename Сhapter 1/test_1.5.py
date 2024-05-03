ae23a2e0ed5147950a9062b2c7d2c497556c0d79
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2024-05-03 15:17:43 +0300

    add test 1.5 this text

---
+++ Сhapter 1/test_1.5.py
@@ -0,0 +1,17 @@
+a, b, c = int(input()), int(input()), int(input())
+maxN = a
+minN = c
+
+if b >= maxN and b >= c:
+    maxN = b
+elif c >= maxN and c >= maxN:
+    maxN = c
+
+if b <= minN and b <= a:
+    minN = b
+elif a <= minN and a <= minN:
+    minN = a
+
+averageN = (a+b+c) - maxN - minN
+
+print(maxN, '\n', minN, '\n', averageN)
