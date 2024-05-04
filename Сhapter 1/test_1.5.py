cd3e3556e47f3a61c0f6c360d36c6257fa3ea3a2
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2024-05-04 17:00:10 +0300

    upgrade test 1.5

--- Сhapter 1/test_1.5.py
+++ Сhapter 1/test_1.5.py
@@ -2,12 +2,12 @@ a, b, c = int(input()), int(input()), int(input())
 maxN = a
 minN = c
 
-if b >= maxN:  #and b >= c:
+if b >= maxN and b >= c:
     maxN = b
 elif c >= maxN:
     maxN = c
 
-if b <= minN:  #and b <= a:
+if b <= minN and b <= a:
     minN = b
 elif a <= minN:
     minN = a
